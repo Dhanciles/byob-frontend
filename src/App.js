318cce92ba6e3b822fe69d22055d1beb8f028c1e
Mike Duke <aithon@gmail.com>
2019-02-07 12:30:22 -0700

    Change aside back to header

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   render() { 
     return (
       <div className="app-container">
-        <aside className="doc-aside">
+        <header className="doc-aside">
         <span className="logo-container">
           <h1 className="logo-text">Taco Restaurants API</h1>
         </span>
           <h3 className="header-subtext">Find Top Taco Restaurants Across the US!</h3>
-        </aside>
+        </header>
         <EndPointContainer />
       </div>
     );
