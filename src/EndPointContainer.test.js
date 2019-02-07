ed06374542e48bce596c732319d38031d023e373
Mike Duke <aithon@gmail.com>
2019-02-07 11:41:49 -0700

    Add uuid mock to EndPointContainer.test

--- src/EndPointContainer.test.js
+++ src/EndPointContainer.test.js
@@ -1,6 +1,11 @@
 import React from 'react'; 
 import { shallow } from 'enzyme'; 
-import { EndPointContainer } from './EndPointContainer.js'; 
+import { EndPointContainer } from './EndPointContainer.js';
+import uuid from 'uuid' ;
+
+jest.mock('uuid', () => {
+  return () => {return 1}
+});
 
 describe('EndPointContainer', () => {
   it('should match snapshot', () => {
